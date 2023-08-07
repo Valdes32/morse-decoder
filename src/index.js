7870b8845c220783661fcd92dfae7223368ed960
Valdes32 <pro100kul@gmail.com>
2023-08-07 16:21:37 +0300

    complite

--- src/index.js
+++ src/index.js
@@ -36,11 +36,75 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
+const MORSE_TABLE_TWO = {
+	'0000001011': 'a',
+	'0011101010': 'b',
+	'0011101110': 'c',
+	'0000111010': 'd',
+	'0000000010': 'e',
+	'0010101110': 'f',
+	'0000111110': 'g',
+	'0010101010': 'h',
+	'0000001010': 'i',
+	'0010111111': 'j',
+	'0000111011': 'k',
+	'0010111010': 'l',
+	'0000001111': 'm',
+	'0000001011': 'n',
+	'0000111111': 'o',
+	'0010111110': 'p',
+	'0011111011': 'q',
+	'0000101110': 'r',
+	'0000101010': 's',
+	'0000000011': 't',
+	'0000101011': 'u',
+	'0010101011': 'v',
+	'0000101111': 'w',
+	'0011101011': 'x',
+	'0011101111': 'y',
+	'0011111010': 'z',
+	'1011111111': '1',
+	'1010111111': '2',
+	'1010101111': '3',
+	'1010101011': '4',
+	'1010101010': '5',
+	'1110101010': '6',
+	'1111101010': '7',
+	'1111111010': '8',
+	'1111111110': '9',
+	'1111111111': '0',
+	'**********': ' ',
+};
 
 function decode(expr) {
-    // write your solution here
+	const chunkSize = 10;
+
+	const chunks = [];
+	for (let i = 0; i < expr.length; i += chunkSize) {
+			chunks.push(expr.slice(i, i + chunkSize));
+	}
+
+	let decodedMessage = '';
+	for (let chunk of chunks) {
+			if (chunk === '**********') {
+					decodedMessage += ' ';
+			} else {
+					let morseChar = '';
+					for (let i = 0; i < chunk.length; i += 2) {
+							const morseSymbol = chunk.slice(i, i + 2);
+							if (morseSymbol === '10') {
+									morseChar += '.';
+							} else if (morseSymbol === '11') {
+									morseChar += '-';
+							}
+					}
+					decodedMessage += MORSE_TABLE[morseChar];
+			}
+	}
+
+	return decodedMessage;
 }
 
 module.exports = {
-    decode
-}
+	decode
+};
